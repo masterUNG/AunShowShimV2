237c714e589463313d0592e03db1220a6fa98c08
masterUNG <phrombutr@gmail.com>
2019-10-03 10:28:20 +0700

    Hello

---
+++ lib/screens/my_style.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyStyle {
+
+  // Field
+  Color textColor = Color.fromARGB(0xFF, 0xF6, 0x9B, 0x77);
+  Color mainColor = Color.fromARGB(67, 75, 86, 1);
+  double h1 = 30.0;
+  double h2 = 18.0;
+
+  // Constructor
+  MyStyle();
+  
+}
---
+++ lib/screens/register.dart
@@ -0,0 +1,96 @@
+import 'package:aunshowshim/screens/my_style.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatefulWidget {
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  // Explicit
+  String name, email, password;
+  final formKey = GlobalKey<FormState>();
+
+  // Method
+  Widget registerButton() {
+    return IconButton(
+      icon: Icon(Icons.cloud_upload),
+      tooltip: "Upload Value to my Database",
+      onPressed: () {
+        formKey.currentState.save();
+        print("name = $name, email = $email, password = $password");
+      },
+    );
+  }
+
+  Widget nameText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.face,
+          size: 36.0,
+        ),
+        labelText: "Display Name :",
+        helperText: "Type Your Name",
+      ),
+      onSaved: (String value) {
+        name = value.trim();
+      },
+    );
+  }
+
+  Widget emailText() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.email,
+          size: 36.0,
+        ),
+        labelText: "Email :",
+        helperText: "Type Your Email",
+      ),
+      onSaved: (String value) {
+        email = value.trim();
+      },
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+        ),
+        labelText: "Password :",
+        helperText: "Type Your Password",
+      ),
+      onSaved: (String value) {
+        password = value.trim();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyStyle().textColor,
+        title: Text("Register"),
+        actions: <Widget>[registerButton()],
+      ),
+      body: Form(
+        key: formKey,
+        child: ListView(
+          padding: EdgeInsets.all(40.0),
+          children: <Widget>[
+            nameText(),
+            emailText(),
+            passwordText(),
+          ],
+        ),
+      ),
+    );
+  }
+}
